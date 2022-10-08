a60c59f476ce54e3218880d7e8ba6033d7401763
league <league@imac-21.attlocal.net>
2022-10-07 18:26:55 -0700

    rgfvd

--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,18 @@
 
 void setup() {
+  PImage face = loadImage("face.jpeg");
+  size(1000,1000);
+  face.resize(1000,1000);
+  image(face, 0, 0);
+  
 
 }
 void draw() {
 
+fill(255,mouseX,mouseY);
+ellipse(185,475,200,200);
+ellipse(465,550,200,200);
+fill(5,5,5);
+ellipse(185,475,100,100);
+ellipse(465,550,100,100);
 }
---
+++ src/_05_for_loops/_2_badgers/For_loops.java
@@ -0,0 +1,30 @@
+package _05_for_loops._2_badgers;
+
+public class For_loops {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 2;i++) {
+			for(int k = 0; k < 12;k++) {
+				System.out.print("Badger, ");
+				
+					
+				
+			}
+				for(int j = 0; j < 2;j++) {
+					System.out.print("Mushroom");
+					if(j == 0) {
+						System.out.print(", ");
+					}
+					if(j == 1) {
+						System.out.println();
+					}
+				
+				
+				}
+			
+		}
+
+		System.out.println("Snake!!");}
+}
+	
+
